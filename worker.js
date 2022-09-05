26dc7df8d61471696712ab51130fbe13305bbf66
Nathan Clevenger <nateclev@gmail.com>
2022-09-05 13:18:04 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 export default {
   fetch: (req, env) => {
     const { origin, pathname } = new URL(req.url)
-    const [ _, range, target ] = pathname.split('/')
-    return fetch('https://' + target, { headers: { range }})
+    const [ _, range, ...target ] = pathname.split('/')
+    return fetch('https://' + target.join('/'), { headers: { range }})
   }
 }
